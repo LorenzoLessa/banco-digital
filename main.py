0f35e6a514a68076a167f2d4813c4c3006da3fa7
Lorenzo Lessa <109671787+LorenzoLessa@users.noreply.github.com>
2022-11-25 11:23:53 -0300

    Create main.py
    Criando a classe Cliente e seus atributos + classe ContaCorrente com seus atributos e seus métodos

---
+++ main.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+class Cliente:
+    def __init__(self, nome, cpf, profissao):
+        self.nome = nome
+        self.cpf = cpf
+        self.profissao = profissao
+
+class ContaCorrente:
+    total_contas_criadas = 0
+
+    def __init__(self, cliente, agencia, numero):
+        self.saldo = 100
+        self.cliente = cliente
+        self.agencia = agencia
+        self.numero = numero
+        ContaCorrente.total_contas_criadas += 1
+
+    def transferir(self, valor, destino):
+        destino.depositar(valor)
+
+    def sacar(self, valor):
+        self.saldo -= valor
+
+    def depositar(self, valor):
+        self.saldo += valor
